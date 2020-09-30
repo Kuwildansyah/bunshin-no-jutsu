4ed82c8a6a0cccda51153c20b0a3cc9a288a20df
kukuh wildan <kuwildansyah@gmail.com>
2020-09-30 15:43:12 +0700

    lalala
    oy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component,useState,useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+    const [data, setData] = useState({});   
+
+useEffect(() => {
+  fetch(`http://127.0.0.1:8000/api/users/`)
+  .then(res => res.json())
+  .then(data => {
+    setData(data.name),
+    console.log(data)})
+}, [data])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="background">
+      <div className="hasil">
+        {data.name}
+      </div>
+     </div>
     </div>
-  );
+  )
 }
 
 export default App;
